2f4b6df3393490507cce165962865f77b5b8d81b
Kam1Cadzee <39504960+Kam1Cadzee@users.noreply.github.com>
2019-05-28 01:08:11 +0300

    Updates

---
+++ precache-manifest.9db1bebb387e6ac498703d9968607adf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: 'ddb956f391aa6ef5445ec58c09282948',
+    url: '/goit-react-hw-04-reader/index.html',
+  },
+  {
+    revision: '1be84a3c062c19f9beaf',
+    url: '/goit-react-hw-04-reader/static/css/main.2b9f434b.chunk.css',
+  },
+  {
+    revision: '25c9c600b959a836dfe9',
+    url: '/goit-react-hw-04-reader/static/js/2.4072c871.chunk.js',
+  },
+  {
+    revision: '1be84a3c062c19f9beaf',
+    url: '/goit-react-hw-04-reader/static/js/main.1dcdd2d6.chunk.js',
+  },
+  {
+    revision: '84bf89e08b01550b9b7d',
+    url: '/goit-react-hw-04-reader/static/js/runtime~main.860f1cd1.js',
+  },
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,15 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
 importScripts(
-  "/goit-react-hw-04-reader/precache-manifest.c854cdbde4669b6e81997f9029b80838.js"
+  'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js',
 );
 
-self.addEventListener('message', (event) => {
+importScripts(
+  '/goit-react-hw-04-reader/precache-manifest.9db1bebb387e6ac498703d9968607adf.js',
+);
+
+self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
@@ -33,7 +35,9 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-04-reader/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
-});
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL('/goit-react-hw-04-reader/index.html'),
+  {
+    blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
+  },
+);
